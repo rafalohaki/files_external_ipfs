bb9ccb75e92427124e161a2bb49967dcf9d1ee13
rafalohaki <rafalohaki@gmail.com>
2023-06-08 20:10:45 +0200

    Update IPFS.php

--- lib/Storage/IPFS.php
+++ lib/Storage/IPFS.php
@@ -3,7 +3,7 @@
 namespace OCA\Files_External_IPFS\Storage;
 
 use League\Flysystem\FileNotFoundException;
-use OC\Files\Storage\Flysystem;
+use Pdsinterop\Flysystem\Nextcloud\NextcloudAdapter;
 use OC\Files\Storage\PolyFill\CopyDirectory;
 
 /**
@@ -11,38 +11,41 @@ use OC\Files\Storage\PolyFill\CopyDirectory;
  * @package OCA\Files_External_IPFS\Storage
  * @author V31L <veil@mail.ch>
  */
-class IPFS extends Flysystem {
-	use CopyDirectory;
+class IPFS extends NextcloudAdapter {
+    use CopyDirectory;
 
-	private $adapter, $api;
-	protected $root;
+    private $adapter, $api;
+    protected $root;
 
-	public function __construct($params) {
-		if (isset($params['host'])) {
-			$this->root = isset($params['root']) ? "/{$params['root']}" : '/';
-			$this->api = $params['host'];
+    public function __construct($params) {
+        if (isset($params['host'])) {
+            $this->root = isset($params['root']) ? "/{$params['root']}" : '/';
+            $this->api = $params['host'];
 
-			$this->adapter = new Adapter($this->api, $this->root);
-			$this->buildFlySystem($this->adapter);
-		} else {
-			throw new \Exception('Creating \OCA\Files_External_IPFS\IPFS storage failed');
-		}
-	}
+            // Example: Create an instance of NextcloudAdapter with a given IStorage instance
+            $storageInstance = new NextcloudAdapter($storageInstance);
+            $this->adapter = $storageInstance;
 
-	public function __destruct() {
-	}
+            $this->buildFlySystem($this->adapter);
+        } else {
+            throw new \Exception('Creating \OCA\Files_External_IPFS\IPFS storage failed');
+        }
+    }
 
-	/**
-	 * Check for dependencies (as of now none)
-	 *
-	 * @return array|bool array of missing dependencies or true
-	 */
-	public static function checkDependencies() {
-		$deps = [];
-		return count($deps) == 0 ? true : $deps;
-	}
+    public function __destruct() {
+    }
 
-	public function getId() {
-		return "IPFS::{$this->api}#{$this->root}";
-	}
+    /**
+     * Check for dependencies (as of now none)
+     *
+     * @return array|bool array of missing dependencies or true
+     */
+    public static function checkDependencies() {
+        $deps = [];
+        return count($deps) == 0 ? true : $deps;
+    }
+
+    public function getId() {
+        return "IPFS::{$this->api}#{$this->root}";
+    }
 }
